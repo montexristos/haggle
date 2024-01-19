2540857cc0bc9d58f01d31a840537609b36b3cef
Christos Skevis <c.skevis@opap.gr>
2024-01-19 21:32:36 +0200

    refactor ml functions

--- database/database.go
+++ database/database.go
@@ -5,9 +5,15 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	"time"
 )
 
+var instance *gorm.DB
+
 func GetDb() *gorm.DB {
+	if instance != nil {
+		return instance
+	}
 	//CREATE SCHEMA `haggle` DEFAULT CHARACTER SET utf8 ;
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:123@(localhost:6602)/haggle?charset=utf8&parseTime=True&loc=Local"
@@ -17,6 +23,14 @@ func GetDb() *gorm.DB {
 	if err != nil {
 		panic(err.Error())
 	}
+	instance = db
+	sqlDB, err := db.DB()
+	if err != nil {
+		// control error
+	}
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 	err = db.AutoMigrate(&models.Event{}, &models.Market{}, &models.Selection{})
 	if err != nil {
 		panic(err.Error())
